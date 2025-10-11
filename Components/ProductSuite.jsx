ad8469c5cad13a6db1c62ceec46ab669b49f859e
efeaizesogie <efeaizesogie@gmail.com>
2025-10-11 17:53:45 +0100

    edit product suite

--- Components/ProductSuite.jsx
+++ Components/ProductSuite.jsx
@@ -2,16 +2,29 @@ import Image from 'next/image';
 import productSuiteImg from "../public/images/productsuite.png";
 import dashboardImg from "../public/images/psdashboard.png";
 
-import image1 from "../public/images/device5muni_1.png";
-import image2 from "../public/images/device5muni_2.png";
-
-
-import TownOfParadise from "../public/serving-logos/townOfParadise.jsx";
 import TitleHead from '@/constants/TitleHead';
+import { productSuiteData } from '@/constants/productSuiteData';
+
+const SuiteCard = ({ icon, title, description, index }) => {
+    return (
+        <div className={`'border-l border-white ' ${index === 0 ? " " : ""
+            }`}>
+            <div
+                className={`flex flex-col gap-[14px] p-4 lg:p-6 text-white items-start text-start`}
+            >   <div className='flex gap-2 items-center'>
+                    {icon}
+                    <h3 className="font-semibold text-lg text-balance">{title}</h3>
+                </div>
+                <p className="text-sm font-medium text-white text-pretty">{description}</p>
+            </div>
+        </div>
+
+    );
+}
 
 const ProductSuite = () => {
     return (
-        <div className='flex flex-col w-full min-h-screen items-center relative overflow-hidden'>
+        <div className='flex flex-col w-full h-full 2xl:h-screen items-center relative overflow-hidden bg-[#14241F]'>
             <Image
                 src={productSuiteImg}
                 alt="Muni Premium Background"
@@ -19,19 +32,22 @@ const ProductSuite = () => {
                 priority
             />
             <div className="w-full max-w-7xl pt-20 md:pt-32 lg:pt-48 pb-16 px-4 md:px-6 lg:px-8 relative z-10">
-
-                {/* <div className="mt-12 md:mt-16 lg:mt-20 text-center flex flex-col items-center relative">
-                    <TitleHead title="Unified Access to Muni’s" titleBreak="Full Suite Experience" tag="Product Suite" />
-                </div> */}
+                <div className=" text-center flex flex-col items-center relative">
+                    <TitleHead title="Unified Access to Muni’s" titleBreak="Full Suite Experience" tag="Product Suite" textWhite />
+                </div>
 
                 <div className="flex lg:flex-row flex-col w-full items-center justify-center">
                     <div className='w-full lg:w-1/2 mb-8 lg:mb-0'>
                         <div className='flex flex-col items-center lg:items-start text-center lg:text-left'>
                             {/* Add content here if needed */}
+                            {productSuiteData.map((consequence, index) => (
+                                <SuiteCard key={index} {...consequence} index={index} />
+                            ))}
                         </div>
                     </div>
 
-                    <div className='w-full lg:w-1/2 flex items-center justify-center'>
+                    <div className=' w-full lg:w-1/2 flex items-center justify-center'>
+                        <Image src={dashboardImg} className='w-full z-[2] ml-[120px] lg:min-w-[472px] lg:mt-[100px] lg:ml-[100px]' />
                         {/* <Image
                             src={dashboardImg}
                             alt="Dashboard"
@@ -39,33 +55,6 @@ const ProductSuite = () => {
                         /> */}
                     </div>
                 </div>
-                {/* <section className='border-t-[1px] border-white border-opacity-[10%] mt-[50px] py-[40px] flex flex-col md:flex-row justify-between items-center w-full px-4 md:px-0 gap-[27px]'>
-                    <div className='flex items-end gap-2 flex-shrink-0'>
-                        <p className='font-medium text-white text-[12px] md:text-[12px] text-center md:text-left whitespace-nowrap'>
-                            Proudly showcasted at:
-                        </p>
-                        <Image src={footerlogo} />
-                    </div>
-
-                    <div className='flex flex-wrap text-white justify-center md:justify-end items-center gap-4 md:gap-6 flex-1 min-w-0'>
-                        <div className='flex items-center gap-2 py-[5px] text-[14px] font-medium'>
-                            <Image src={check} />
-                            <p>Process service requests</p>
-                        </div>
-                        <div className='flex items-center gap-2 py-[5px] text-[14px] font-medium'>
-                            <Image src={check} />
-                            <p>Query all public municipal data</p>
-                        </div>
-                        <div className='flex items-center gap-2 py-[5px] text-[14px] font-medium'>
-                            <Image src={check} />
-                            <p>Receive alerts and notifications</p>
-                        </div>
-                        <div className='flex items-center gap-2 py-[5px] text-[14px] font-medium'>
-                            <Image src={check} />
-                            <p>And much more!</p>
-                        </div>
-                    </div>
-                </section> */}
             </div>
         </div >
     )
--- constants/TitleHead.jsx
+++ constants/TitleHead.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const TitleHead = ({tag, title, titleBreak}) => {
+const TitleHead = ({ tag, title, titleBreak, textWhite }) => {
+
     return (
         <>
-            <p className="text-sm md:text-base text-[#006A4A] font-semibold px-4 md:px-8 max-w-lg mb-4 md:mb-6">{tag}</p>
-            <h1 className="text-2xl md:text-4xl lg:text-[54px] text-[#000000] font-semibold px-4 md:px-8 md:leading-[64px] lg:leading-[64px] leading-tight max-w-4xl">{title} <br/> {titleBreak}</h1>
+            <p className={`${textWhite ? "text-[#C4E76A]" : 'text-[#006A4A]'} "text-sm md:text-base  font-semibold px-4 md:px-8 max-w-lg mb-4 md:mb-6"`}>{tag}</p>
+            <h1 className={` ${textWhite ? "text-[#ffffff]" : "text-[#000000]"} "text-2xl md:text-4xl lg:text-[54px] font-semibold px-4 md:px-8 md:leading-[64px] lg:leading-[64px] leading-tight max-w-4xl"`}>{title} <br /> {titleBreak}</h1>
         </>
     )
 }
