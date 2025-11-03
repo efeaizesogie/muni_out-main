c6bea1f38ded8ec8b6c0f1cf97741ccd47d9a55c
efeaizesogie <efeaizesogie@gmail.com>
2025-11-03 22:55:14 +0100

    add items start to header on mobile

--- src/components/admin/Header.jsx
+++ src/components/admin/Header.jsx
@@ -16,10 +16,10 @@ const Header = ({ heading, para }) => {
             <div className="absolute inset-0 bg-[#14241F] bg-opacity-20 z-1"></div>
 
 
-            <div className='flex flex-col items-center text-center z-10  pt-[100px] md:pt-[150px] w-full lg:w-[80%] mx-[20px] lg:mx-auto h-full relative'>
-                <h1 className='font-semibold text-white md:text-[70px] text-[42px] leading-tight '>{heading}</h1>
-                <p className='font-medium text-white py-[12px] md:py-[24px] text-lg text-center mx-5'>{para}</p>
-                <div className='mb-16 md:mb-[85px]'>
+            <div className='flex flex-col md:items-center md:text-center z-10  pt-[100px] md:pt-[150px] w-full lg:w-[80%] mx-[20px] lg:mx-auto h-full relative'>
+                <h1 className='font-semibold text-white md:text-[70px] text-[42px] mx-5 leading-tight '>{heading}</h1>
+                <p className='font-medium text-white py-[12px] md:py-[24px] text-lg mx-5'>{para}</p>
+                <div className='mb-16 md:mb-[85px] mx-5'>
                     <Button />
                 </div>
             </div>
