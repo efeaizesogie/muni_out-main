dd7c41cf1443e629d92ff35550d054c226228efa
efeaizesogie <efeaizesogie@gmail.com>
2025-11-04 01:08:49 +0100

    `Refactor Header component: update image container and background styles`

--- src/components/admin/Header.jsx
+++ src/components/admin/Header.jsx
@@ -3,17 +3,21 @@ import React from 'react'
 import Button from '../Button'
 import muniPremium from "@/public/muni_premium.png";
 
+
 const Header = ({ heading, para }) => {
     return (
-        <div className='flex flex-col w-full items-center relative overflow-hidden'>
-            <Image
-                src={muniPremium}
-                alt="Muni Premium Background"
-                className="absolute inset-0 w-full h-full object-cover object-top z-0"
-                priority
-                fill
-            />
-            <div className="absolute inset-0 bg-[#14241F] bg-opacity-20 z-1"></div>
+
+        <div className='flex flex-col w-full h-full items-center relative overflow-hidden'>
+            <div className="absolute inset-0 w-full h-full z-0" style={{ minWidth: '1200px' }}>
+                <Image
+                    src={muniPremium}
+                    alt="Muni Premium Background"
+                    fill
+                    className="object-cover object-top"
+                    priority
+                />
+            </div>
+            <div className="absolute inset-0 bg-[#14241F] bg-opacity-0 md:bg-opacity-20 z-1"></div>
 
 
             <div className='flex flex-col md:items-center md:text-center z-10  pt-[100px] md:pt-[150px] w-full lg:w-[80%] mx-[20px] lg:mx-auto h-full relative'>
